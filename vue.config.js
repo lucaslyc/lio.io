18f2c9ddb2592f00c78eeea1472c5b87ff8b0eda
liyangchun <838164254@qq.com>
2024-10-08 16:31:02 +0800

    99

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // 如果应用被部署在一个子路径下，你需要在这里指定子路径
   // 例如，如果你的应用被部署在 https://www.my-domain.com/my-app/
   // 那么将这个值改为 '/my-app/'
-  publicPath: process.env.NODE_ENV === 'production' ? '/lucaslyc.github.io/lio.io/lio.io/' : '/'
+  publicPath: './'
 }
 
 
